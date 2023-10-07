fc7238b2af03dfc1eec62da648158ffc3fe79c14
Alex verezubov <60190100+Bulbach@users.noreply.github.com>
2023-10-07 13:27:58 +0300

    feature:three change next_release

--- src/main/java/org/example/nextRelease/FeatureNextRelease.java
+++ src/main/java/org/example/nextRelease/FeatureNextRelease.java
@@ -2,9 +2,18 @@ package org.example.nextRelease;
 
 public class FeatureNextRelease {
     private String purum;
+    private String burum;
 
     public String getPurum() {
         return purum;
+    }
+
+    public String getBurum() {
+        return burum;
+    }
+
+    public void setBurum(String burum) {
+        this.burum = burum;
     }
 
     public void setPurum(String purum) {
