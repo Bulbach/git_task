39a05559160e46cc78b180dd00a9ecd33f3ce1e2
Alex verezubov <60190100+Bulbach@users.noreply.github.com>
2023-10-07 12:33:28 +0300

    feature: two change Next_release

--- src/main/java/org/example/nextRelease/FeatureNextRelease.java
+++ src/main/java/org/example/nextRelease/FeatureNextRelease.java
@@ -1,4 +1,13 @@
 package org.example.nextRelease;
 
 public class FeatureNextRelease {
+    private String purum;
+
+    public String getPurum() {
+        return purum;
+    }
+
+    public void setPurum(String purum) {
+        this.purum = purum;
+    }
 }
