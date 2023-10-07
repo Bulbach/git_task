ab3349cf2cb53159a67d4b759af52ca08cff5e6c
Alex verezubov <60190100+Bulbach@users.noreply.github.com>
2023-10-07 12:22:13 +0300

    feature: two change future_release

--- src/main/java/org/example/feature/FeatureFuture.java
+++ src/main/java/org/example/feature/FeatureFuture.java
@@ -1,4 +1,13 @@
 package org.example.feature;
 
 public class FeatureFuture {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
