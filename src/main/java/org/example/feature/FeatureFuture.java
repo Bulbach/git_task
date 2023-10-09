a94d493891e9c0eae9f45eeeebdd4b0a60edcc13
Alex verezubov <60190100+Bulbach@users.noreply.github.com>
2023-10-09 13:46:57 +0300

    feature:third change future

--- src/main/java/org/example/feature/FeatureFuture.java
+++ src/main/java/org/example/feature/FeatureFuture.java
@@ -2,6 +2,15 @@ package org.example.feature;
 
 public class FeatureFuture {
     private String name;
+    private String target;
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
 
     public String getName() {
         return name;
