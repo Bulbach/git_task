9bb2cc6d5118a1d4ba209f6969f13a539d122894
Alex verezubov <60190100+Bulbach@users.noreply.github.com>
2023-10-09 14:00:35 +0300

    feature:first commit branch feature/next_release_2.0

--- src/main/java/org/example/release/Release.java
+++ src/main/java/org/example/release/Release.java
@@ -1,8 +1,19 @@
 package org.example.release;
 
+import java.util.List;
+
 public class Release {
     private String dateRelease;
     private String bugFix;
+    private List<String> changes;
+
+    public List<String> getChanges() {
+        return changes;
+    }
+
+    public void setChanges(List<String> changes) {
+        this.changes = changes;
+    }
 
     public Release(String dateRelease, String bugFix) {
         this.dateRelease = dateRelease;
